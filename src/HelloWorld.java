c0749d02acb810c9311512a54b155540d5b66947
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-09-16 16:59:00 -0500

    basic java excercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,58 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.print("Hello");
+        System.out.print(" World!");
+
+        int myFavoriteNumber = 7;
+
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.14159";
+
+        System.out.println(myString.charAt(0));
+
+        float myNumber = 3.14F;
+
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+        System.out.println(three);
+
+        int three = (int) "three";
+
+        int x = 4;
+        x += 5;
+
+        System.out.println(x);
+
+        int x = 3;
+        int y = 4;
+        y *= x ;
+
+        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x ;
+
+        System.out.println(y);
+
+
+
+    }
+
+}
