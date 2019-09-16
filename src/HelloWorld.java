d6291c750141dafacdf8c6dddee63f48b92f0dc6
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-09-16 17:00:50 -0500

    basic java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,9 +17,9 @@ public class HelloWorld {
 
         System.out.println(myNumber);
 
-        int x = 5;
-        System.out.println(x++);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
 
         int x = 5;
         System.out.println(++x);
@@ -31,25 +31,25 @@ public class HelloWorld {
 
         System.out.println(three);
 
-        int three = (int) "three";
+//        int three = (int) "three";
 
-        int x = 4;
-        x += 5;
-
-        System.out.println(x);
-
-        int x = 3;
-        int y = 4;
-        y *= x ;
-
-        System.out.println(y);
-
-        int x = 10;
-        int y = 2;
-        x /= y;
-        y -= x ;
-
-        System.out.println(y);
+//        int x = 4;
+//        x += 5;
+//
+//        System.out.println(x);
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x ;
+//
+//        System.out.println(y);
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x ;
+//
+//        System.out.println(y);
 
 
 
