a5398dc0b1287bf18a3a9455fa07a4f19f19e1be
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-09-26 14:06:08 -0500

    complete array exercises

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,15 +1,16 @@
 import java.util.Random;
 
 public class ServerNameGenerator {
+    public static String[] adjectives = {"crashing", "rhythmic", "fast", "acidic", "billions", "melodic", "microscopic", "lazy", "fierce", "defeated"};
+
+    public static String[] nouns = {"ship", "hero", "fish", "dog", "cat", "world", "student", "business", "night", "man"};
+
     static String randomCombo(String [] random){
-        int rdn = new Random().nextInt(random.length);
-        return random[rdn];
+        int rnd = new Random().nextInt(random.length);
+        return random[rnd];
     }
 
-
     public static void main(String[] args) {
-    String[] adjectives = {"crashing", "rhythmic", "fast", "acidic", "billions", "melodic", "microscopic", "lazy", "fierce", "defeated"};
-    String[] nouns = {"ship", "hero", "fish", "dog", "cat", "world", "student", "business", "night", "man"};
         System.out.println("Here is your server name: ");
         System.out.println(randomCombo(adjectives)+ "-" + randomCombo(nouns));
 
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,15 +1,91 @@
 package movies;
 
-import java.util.Arrays;
+import util.Input;
 
 public class MoviesApplication {
 
     public static void main(String[] args) {
 
-        Movie movie = new Movie("", "");
+        Input input = new Input();
+        System.out.println("Welcome to the movie app");
+        int userInput;
+        boolean willContinue;
+        do {
+            displayMenu();
 
-        System.out.println(movie.getMovieName());
+            userInput = input.getInt(0, 5);
 
+            willContinue = processChoice();
+
+            System.out.println(userInput);
+
+        }while(willContinue);
+
+
+    }
+
+    public static void displayMenu(){
+        String menu = "What would you like to do?\n" +
+                "\n" +
+                "0 - exit\n" +
+                "1 - view all movies\n" +
+                "2 - view movies in the animated category\n" +
+                "3 - view movies in the drama category\n" +
+                "4 - view movies in the horror category\n" +
+                "5 - view movies in the scifi category";
+
+        System.out.println(menu);
+    }
+
+    public static boolean processChoice(int choice){
+        boolean output = true;
+
+        switch (choice){
+
+            case 0:
+                System.out.println("Exit");
+                output = false;
+                break;
+            case 1:
+                displayMovies();
+                break;
+            case 2:
+                displayMovies("animated");
+                break;
+            case 3:
+                displayMovies("drama");
+                break;
+            case 4:
+                displayMovies("horror");
+                break;
+            case 5:
+                displayMovies("scifi");
+                break;
+        }
+        return output;
+    }
+
+    public static void displayMovies(){
+        String output = "";
+
+        for (Movie movie : MoviesArray.findAll()){
+            output += String.format("%s -- %s%n", movie.getMovieName(), movie.getMovieCategory());
+        }
+
+        System.out.println(output);
+    }
+
+    public static void displayMovies(String category){
+        String output = "";
+
+        for (Movie movie : MoviesArray.findAll()){
+            if (category.equalsIgnoreCase(movie.getMovieCategory())){
+            output += String.format("%s -- %s%n", movie.getMovieName(), movie.getMovieCategory());
+
+            }
+        }
+
+        System.out.println(output);
     }
 
 }
