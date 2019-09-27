f8cc3293516b7c27107a80a9a4e3ea8fde7300df
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-09-27 15:31:42 -0500

    Interfaces and Abstract Classes exercises complete

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,9 +3,10 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-        Rectangle box1  = new Rectangle(5,4);
 
-        Rectangle box2 = new Square(5);
+        Measurable box1  = new Rectangle(5,4);
+
+        Measurable box2 = new Square(5);
 
         System.out.println("box1.getArea() = " + box1.getArea());
         System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
@@ -15,6 +16,20 @@ public class ShapesTest {
 
 
 
+
+        Measurable myShape = new Rectangle(5, 4);
+        Measurable myShape1 = new Square(5);
+
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+
+        System.out.println("myShape1.getArea() = " + myShape1.getArea());
+        System.out.println("myShape1.getPerimeter() = " + myShape1.getPerimeter());
+
+
+
+
     }
+
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,19 +1,29 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
+
 
     public Square(int side){
         super(side, side);
     }
 
     @Override
-    public int getPerimeter() {
-        return 4 * super.length;
+    public double setWidth() {
+        return this.width;
     }
 
     @Override
-    public int getArea() {
-        return super.length * super.length;
+    public double setLength() {
+        return this.length;
     }
 
+    @Override
+    public double getPerimeter() {
+        return 4 * this.length;
+    }
+
+    @Override
+    public double getArea() {
+        return this.length * this.width;
+    }
 }
